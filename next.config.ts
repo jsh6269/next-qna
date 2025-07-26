a9cc5487c5344f40e8011d1c32ba3ea97a8617c9
suhan8984 <suhan8984@daum.net>
2025-07-26 18:33:49 +0900

    fix: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const isVercel = !!process.env.VERCEL_URL;
-
-const nextConfig: NextConfig = {
-  env: {
-    NEXTAUTH_URL: isVercel
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
